640fa5b95b6b8961147f444acfb1d5e9af196192
Hugoag03 <hugoarevalogil08@gmail.com>
2024-05-29 10:30:58 +0200

    Primer commit

---
+++ src/main/java/com/springboot/hospital/ApiHospitalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHospitalSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHospitalSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/hospital/ApiHospitalSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHospitalSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
